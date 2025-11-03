b9761bb9c504bffdeefd2f2eb9cdf3c90e57ef22
Kural <kuralarasan.kumar@ugent.be>
2025-11-03 13:52:23 +0100

    Refactor Chi-square test evaluation: streamline data matrix initialization and remove redundant solution file

--- Chi_Square_Test/evaluation/test.R
+++ Chi_Square_Test/evaluation/test.R
@@ -1,12 +1,12 @@
+# Pre-load the data matrix in global environment
+data_matrix <- matrix(c(45, 25, 15, 35, 20, 40), 
+                      nrow = 2, 
+                      dimnames = list(
+                        Geslacht = c("Man", "Vrouw"),
+                        Misdrijf = c("Diefstal", "Geweld", "Fraude")
+                      ))
+
 context({
-  # Pre-load the data matrix for students
-  data_matrix <- matrix(c(45, 25, 15, 35, 20, 40), 
-                        nrow = 2, 
-                        dimnames = list(
-                          Geslacht = c("Man", "Vrouw"),
-                          Misdrijf = c("Diefstal", "Geweld", "Fraude")
-                        ))
-  
   testcase("Chi-square test uitvoeren", {
     # Test that student calls chisq.test on data_matrix
     testEqual(NULL, function(env) { 
@@ -17,9 +17,10 @@ context({
   
   testcase("Juiste functie gebruikt", {
     # Verify the chi-square statistic is correct
-    result <- chisq.test(data_matrix)
+    expected_result <- chisq.test(data_matrix)
     testEqual(NULL, function(env) { 
-      round(env$chisq.test(data_matrix)$statistic, 4) 
-    }, round(result$statistic, 4))
+      result <- env$chisq.test(data_matrix)
+      round(result$statistic, 4) 
+    }, round(expected_result$statistic, 4))
   })
 })
--- Chi_Square_Test/solution/test.R
+++
@@ -1,3 +0,0 @@
-# The data matrix is already provided
-# Students just need to call the chi-square test function
-chisq.test(data_matrix)
