faf10caf9b306c10b9092318aa54cb32a7c5709c
Kural <kuralarasan.kumar@ugent.be>
2025-11-04 11:20:45 +0100

    Refactor dataset loading test to return the dataset directly for improved clarity

--- Hoofdstuk_1_De logica van statistische vergelijkingen en analyses/1.4 Dataset Verkenning - Veiligheidsgevoel/evaluation/Answer.R
+++ Hoofdstuk_1_De logica van statistische vergelijkingen en analyses/1.4 Dataset Verkenning - Veiligheidsgevoel/evaluation/Answer.R
@@ -1,6 +1,5 @@
 # Load the dataset from the working directory to get expected result
 source("../workdir/load_data.R")
-expected_dataset <- df_fear_of_crime_gent
 
 context({
   testcase("str(df_fear_of_crime_gent)", {
@@ -8,8 +7,8 @@ context({
       # Students should source the load_data.R file from their working directory
       env$source("load_data.R")
       
-      # Return the class to verify dataset is loaded correctly
-      class(env$df_fear_of_crime_gent)
-    }, class(expected_dataset))
+      # Just return the dataset itself, like Echo does with simple values
+      env$df_fear_of_crime_gent
+    }, df_fear_of_crime_gent)
   })
 })
