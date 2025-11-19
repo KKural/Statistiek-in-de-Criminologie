f9c566523a98896e82a62004e733f233b15ddb2a
Kural <kuralarasan.kumar@ugent.be>
2025-11-19 15:56:50 +0100

    Refactor evaluation tests in Answer.R to remove description fields for consistency and improved readability

--- Hoofdstuk_2_Inleidende begrippen/Oef - 2.8 Variabeletype - Studentnummer classificatie/evaluation/Answer.R
+++ Hoofdstuk_2_Inleidende begrippen/Oef - 2.8 Variabeletype - Studentnummer classificatie/evaluation/Answer.R
@@ -1,51 +1,36 @@
 context({
-  testcase(
-    description = "Favoriete keuze voor online onderzoek",
-    {
-      testEqual(
-        expected = "nominaal",
-        actual = if(exists("favoriete_keuze")) tolower(favoriete_keuze) else "niet gevonden"
-      )
-    }
-  )
+  testcase({
+    testEqual(
+      "nominaal",
+      if(exists("favoriete_keuze")) tolower(favoriete_keuze) else "niet gevonden"
+    )
+  })
   
-  testcase(
-    description = "Leeftijd",
-    {
-      testEqual(
-        expected = "ratio", 
-        actual = if(exists("leeftijd")) tolower(leeftijd) else "niet gevonden"
-      )
-    }
-  )
+  testcase({
+    testEqual(
+      "ratio", 
+      if(exists("leeftijd")) tolower(leeftijd) else "niet gevonden"
+    )
+  })
   
-  testcase(
-    description = "Geslacht",
-    {
-      testEqual(
-        expected = "nominaal",
-        actual = if(exists("geslacht")) tolower(geslacht) else "niet gevonden"
-      )
-    }
-  )
+  testcase({
+    testEqual(
+      "nominaal",
+      if(exists("geslacht")) tolower(geslacht) else "niet gevonden"
+    )
+  })
   
-  testcase(
-    description = "Studierichting", 
-    {
-      testEqual(
-        expected = "nominaal",
-        actual = if(exists("studierichting")) tolower(studierichting) else "niet gevonden"
-      )
-    }
-  )
+  testcase({
+    testEqual(
+      "nominaal",
+      if(exists("studierichting")) tolower(studierichting) else "niet gevonden"
+    )
+  })
   
-  testcase(
-    description = "Studentnummer",
-    {
-      testEqual(
-        expected = "nominaal",
-        actual = if(exists("studentnummer")) tolower(studentnummer) else "niet gevonden"
-      )
-    }
-  )
+  testcase({
+    testEqual(
+      "nominaal",
+      if(exists("studentnummer")) tolower(studentnummer) else "niet gevonden"
+    )
+  })
 })
