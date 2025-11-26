9ae26e49a773ca735f49a5b074a045f1fd89ad80
Kural <kuralarasan.kumar@ugent.be>
2025-11-26 15:56:21 +0100

    Add TRUE assertion to testEqual for werkuren statistieken evaluation

--- Hoofdstuk_3_De univariate beschrijvende statistiek/Oef - 3.3/evaluation/Answer.R
+++ Hoofdstuk_3_De univariate beschrijvende statistiek/Oef - 3.3/evaluation/Answer.R
@@ -4,6 +4,7 @@ context({
     {
       testEqual(
         "Controleer je stap-voor-stap antwoorden voor werkuren statistieken",
+        TRUE,
         function(env) {
           results <- list()
           
