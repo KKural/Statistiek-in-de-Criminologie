b7087177db4a697bc8ea85ba3205e751f0658db0
Kural <kuralarasan.kumar@ugent.be>
2026-09-03 08:58:53 +0200

    udpated again 1.8 and 2.2

--- Hoofdstuk_1_De logica van statistische vergelijkingen en analyses/Oef - 1.8 Correlatie vs. Causatie/evaluation/Answer.R
+++ Hoofdstuk_1_De logica van statistische vergelijkingen en analyses/Oef - 1.8 Correlatie vs. Causatie/evaluation/Answer.R
@@ -14,7 +14,7 @@ context({
               "De gevonden **samenhang** toont niet dat straatverlichting eerst veranderde en daarna de criminaliteit verminderde. Andere factoren, zoals **politie-inzet** of **buurtwelvaart**, kunnen beide variabelen beïnvloeden.\n\n",
               "**Controleer:**\n",
               "Vermeldt het onderzoek dat de verlichting doelgericht werd veranderd en dat andere verklaringen werden gecontroleerd? Zo niet, welke optie beschrijft alleen de samenhang zonder een oorzaak vast te leggen?\n\n",
-              "**In het handboek:** **Deel I, Hoofdstuk 6, p. 115** (**PDF p. 129**) bespreekt expliciet dat statistiek samenhang toont, maar causaliteit niet bewijst.\n\n",
+              "**In het handboek:** **Deel I, Hoofdstuk 6, 115** bespreekt expliciet dat statistiek samenhang toont, maar causaliteit niet bewijst.\n\n",
               "**Lees meer:** [Correlatie en causaliteit](https://maatschappij-wetenschappen.nl/havo/druk2/5-correlatie-en-causaliteit/)"
             ),
             "2" = paste0(
@@ -22,13 +22,13 @@ context({
               "**Omgekeerde causaliteit** is mogelijk, maar het resultaat bewijst deze richting niet. Je vervangt daardoor één onbewezen causale conclusie door een andere.\n\n",
               "**Controleer:**\n",
               "Toont het onderzoek welke variabele eerst veranderde? Als de tijdsvolgorde onbekend is, welke optie laat de causale richting dan open?\n\n",
-              "**In het handboek:** **Deel I, Hoofdstuk 6, p. 115** (**PDF p. 129**) bespreekt expliciet dat statistiek samenhang toont, maar causaliteit niet bewijst.\n\n",
+              "**In het handboek:** **Deel I, Hoofdstuk 6, 115** bespreekt expliciet dat statistiek samenhang toont, maar causaliteit niet bewijst.\n\n",
               "**Lees meer:** [Correlatie en causaliteit](https://maatschappij-wetenschappen.nl/havo/druk2/5-correlatie-en-causaliteit/)"
             ),
             "3" = paste0(
               "**Juist:**\n",
               "Het resultaat toont een **samenhang**, maar levert onvoldoende bewijs om één causale verklaring te kiezen.\n\n",
-              "**In het handboek:** **Deel I, Hoofdstuk 6, p. 115** (**PDF p. 129**) bespreekt expliciet dat statistiek samenhang toont, maar causaliteit niet bewijst.\n\n",
+              "**In het handboek:** **Deel I, Hoofdstuk 6, 115** bespreekt expliciet dat statistiek samenhang toont, maar causaliteit niet bewijst.\n\n",
               "**Lees meer:** [Correlatie en causaliteit](https://maatschappij-wetenschappen.nl/havo/druk2/5-correlatie-en-causaliteit/)"
             ),
             "4" = paste0(
@@ -36,7 +36,7 @@ context({
               "De sterkte van een **correlatie** toont niet dat **derde variabelen** zijn uitgesloten. Ook een sterke samenhang kan ontstaan doordat bijvoorbeeld **buurtwelvaart** zowel de straatverlichting als de criminaliteit beïnvloedt.\n\n",
               "**Controleer:**\n",
               "Staat er dat buurtwelvaart, politie-inzet of andere factoren werden gecontroleerd? Zo niet, welke optie houdt deze alternatieve verklaringen open?\n\n",
-              "**In het handboek:** **Deel I, Hoofdstuk 6, p. 115** (**PDF p. 129**) bespreekt expliciet dat statistiek samenhang toont, maar causaliteit niet bewijst.\n\n",
+              "**In het handboek:** **Deel I, Hoofdstuk 6, 115** bespreekt expliciet dat statistiek samenhang toont, maar causaliteit niet bewijst.\n\n",
               "**Lees meer:** [Correlatie en causaliteit](https://maatschappij-wetenschappen.nl/havo/druk2/5-correlatie-en-causaliteit/)"
             )
           )
